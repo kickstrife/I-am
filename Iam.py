d593846304320889a94209b785760d1f37cac3b6
strife <ciscoedm@gmail.com>
2016-02-09 16:57:56 -0500

    My first python program: I am...
    Every time you run the file, a random word is picked and printed to the
    user, for example, I am Smart, I am Cool. It just randomly picks one.

---
+++ Iam.py
@@ -0,0 +1,24 @@
+import random
+
+Adj = ['Bloody','Disgusted','Rapid','Shut','Utter''Cooperative','Flowery',
+'Tangy','Curvy','Abandoned','Warm','Fertile','Wonderful','Lying','Fair','Fine',
+'Thirsty','Synonymous','Lacking','Unused','Educated','Earthy','Illegal','Acrid',
+'Common','Addicted','Questionable','Lush','Trashy','Pretty','Filthy','Mountainous',
+'Private','Cute','Marvelous','Sour','Unbecoming','Shy','Last','Gorgeous','Rebel',
+'Handy','Agreeable','Magnificent','Workable','Grey','Disastrous','Skinny','Frightened',
+'Quiet','Woozy','Habitual','Uptight','Nostalgic','Difficult','Paltry','Garrulous',
+'Rigid','Puzzled','Waiting','Immense','Tender','Tight','Incompetent','Statuesque',
+'Illustrious','Crabby','Hard','Breezy','Silky','Murky','Brainy','Muddled','Far',
+'Wistful','Onerous','Chemical','Symptomatic','Chief','Husky','Absorbed','Hallowed',
+'Better','Thinkable','Longing','Erect','Alert','Towering','Staking','Arrogant',
+'Nippy','Impolite','Male','Noiseless','Unwritten','Economic','Truculent','Nice',
+'Measly','Awake','Cute','Tangible','Amuck','Numberless','Upbeat','Ignorant',
+'Wholesale','Tacit','Tested','Spotted','Snobbish','Swift','Salty','Distinct',
+'Simple','Damaged','Past','Strong','First','Lively','Grandiose','Giddy','Great',
+'Reminiscent','Tense','Smelly','Pricey','Beneficial','Lonely','Incredible',
+'Tasteful','Inquisitive','Peaceful','Earsplitting','Envious','Encouraging',
+'Zonked','Hollow']
+
+randomate = random.choice(Adj)
+varIAM = 'I am ' + randomate
+print(varIAM)
